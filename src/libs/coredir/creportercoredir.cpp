sailfishos/crash-reporter

commit 341a79909038c5e2579614c7c1dcc2edc6cc5445

[performance] Use QDirIterator in CReporterCoreDir::updateCoreList()
QDir::entryList() turns out to be too slow for large directorires.

src/libs/coredir/creportercoredir.cpp
@@ -189,13 +189,16 @@ void CReporterCoreDir::updateCoreList()
 	qDebug() << __PRETTY_FUNCTION__ << "Refreshing core directory list.";
 
     QDir dir(d->directory);
+    dir.setFilter(QDir::Files | QDir::NoDotAndDotDot);
+    dir.setNameFilters(QStringList() << rcore_file_name_filter << rcore_lzo_file_name_filter);
 
-	QStringList filters;
-    filters << QString(rcore_file_name_filter) << QString(rcore_lzo_file_name_filter);
 	// Remove old entries.
 	d->coresAtDirectory.clear();
-	// Update current entries.
-    d->coresAtDirectory = dir.entryList(filters, QDir::Files | QDir::NoDotAndDotDot, QDir::Time);
+
+    QDirIterator it(dir);
+    while (it.hasNext()) {
+        d->coresAtDirectory.append(it.next());
+    }
 }
 
 // End of file

commit 8c9115aee60f55b1887db0a9e76c001976a647a8

[coredir] Fix CReporterCoreDir::updateCoreList()
coresAtDirectory should contain only file names, but it.next() returns
full path.

Fixes a bug introduced in 341a79909038c5e2579614c7c1dcc2edc6cc5445.

src/libs/coredir/creportercoredir.cpp
@@ -194,7 +194,8 @@ void CReporterCoreDir::updateCoreList()
 
     QDirIterator it(dir);
     while (it.hasNext()) {
-        d->coresAtDirectory.append(it.next());
+        it.next();
+        d->coresAtDirectory.append(it.fileName());
     }
 }
 